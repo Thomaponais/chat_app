4da6884a2c23d9f54b3eb76c7acc45a7d47660c0
Thomaponais <thomas_guehenneux@b-engineer.com>
2021-10-08 00:52:51 +0900

    Add Apollo

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ApolloProvider } from '@apollo/client'
+import { client } from './utils/apollo'
 import GlobalStyles from './GlobalStyles'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
---
+++ src/utils/apollo.tsx
@@ -0,0 +1,50 @@
+import { ApolloClient, InMemoryCache, useQuery, gql, ApolloError } from '@apollo/client'
+
+export const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'https://angular-test-backend-yc4c5cvnnq-an.a.run.app/graphiql',
+})
+
+const FETCH_LATEST_MESSAGES = gql`
+  query ($channelId: String!) {
+    fetchLatestMessages(channelId: $channelId) {
+      id
+      text
+      userId
+      channelId
+      createdAt
+    }
+  }
+`
+
+export const FETCH_MORE_MESSAGES = gql`
+  query ($channelId: String!, $messageId: String!, $old: Boolean!) {
+    fetchMoreMessages(channelId: $channelId, messageId: $messageId, old: $old) {
+      id
+      text
+      userId
+      channelId
+      createdAt
+    }
+  }
+`
+
+export const POST_MESSAGE = gql`
+  mutation ($channelId: String!, $userId: String!, $text: String!) {
+    postMessage(userId: $userId, channelId: $channelId, text: $text) {
+      createdAt
+    }
+  }
+`
+
+export function FetchLatestMessages(channelId: string): {
+  data: any
+  loading: boolean
+  error: ApolloError | undefined
+} {
+  const { loading, error, data } = useQuery(FETCH_LATEST_MESSAGES, {
+    variables: { channelId },
+  })
+
+  return { data, loading, error }
+}
